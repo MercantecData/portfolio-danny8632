59b4e42da081cf90c5a9eee97323f94cec979550
Danny Haslund <dannyhasl1@gmail.com>
2020-01-15 15:00:00 +0100

    I technical places a dark-mode btn, and it changes the theme. But I would like to make a settings menu for it, so I can change it back to white Theme.

--- App_Programmering/App/app/src/main/java/com/example/dannyinteractive/MainActivity.kt
+++ App_Programmering/App/app/src/main/java/com/example/dannyinteractive/MainActivity.kt
@@ -1,14 +1,17 @@
 package com.example.dannyinteractive
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
+
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
@@ -17,6 +20,17 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     lateinit var navView: NavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        var bundle :Bundle ?=intent.extras
+        var theme_id = bundle?.getInt("theme") // 1
+
+        when (theme_id) {
+            1 -> setTheme(R.style.Dark_AppTheme)
+            else -> { // Note the block
+                setTheme(R.style.AppTheme)
+            }
+        }
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
@@ -34,10 +48,24 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         navView.setNavigationItemSelectedListener(this)
     }
 
+
+    fun reload(i: Int = -1) {
+        val intent = intent
+        intent.putExtra("theme", i)
+        finish()
+        overridePendingTransition(
+            android.R.anim.fade_in,
+            android.R.anim.fade_out
+        )
+        startActivity(intent)
+    }
+
+
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.nav_profile -> {
-                Toast.makeText(this, "Profile clicked", Toast.LENGTH_SHORT).show()
+                setTheme(R.style.Dark_AppTheme)
+                reload(1)
             }
             R.id.nav_messages -> {
                 Toast.makeText(this, "Messages clicked", Toast.LENGTH_SHORT).show()

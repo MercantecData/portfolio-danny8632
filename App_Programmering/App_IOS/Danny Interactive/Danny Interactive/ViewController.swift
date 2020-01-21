063bb07c9788cfa2fb1e4981a4600a07f736a8df
Mercantec Elev <elev@VIHCA-HOTMAC-014.local>
2020-01-21 13:23:43 +0100

    Initializing new branch for iOS development

---
+++ App_Programmering/App_IOS/Danny Interactive/Danny Interactive/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Danny Interactive
+//
+//  Created by Mercantec Elev on 21/01/2020.
+//  Copyright © 2020 Mercantec Elev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ App_Programmering/App_IOS/Danny Interactive/Danny InteractiveTests/Danny_InteractiveTests.swift
@@ -0,0 +1,34 @@
+//
+//  Danny_InteractiveTests.swift
+//  Danny InteractiveTests
+//
+//  Created by Mercantec Elev on 21/01/2020.
+//  Copyright © 2020 Mercantec Elev. All rights reserved.
+//
+
+import XCTest
+@testable import Danny_Interactive
+
+class Danny_InteractiveTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}

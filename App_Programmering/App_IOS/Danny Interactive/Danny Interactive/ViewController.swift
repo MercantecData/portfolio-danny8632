38e9b440a5b1ccbf97a00e5bc21ebf7364bdf5de
Mercantec Elev <elev@VIHCA-HOTMAC-014.local>
2020-01-21 14:31:01 +0100

    IOS done

--- App_Programmering/App_IOS/Danny Interactive/Danny Interactive/ViewController.swift
+++ App_Programmering/App_IOS/Danny Interactive/Danny Interactive/ViewController.swift
@@ -10,11 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var leading: NSLayoutConstraint!
+    
+    @IBOutlet weak var trailing: NSLayoutConstraint!
+    
+    var menuShown = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func menuTapped(_ sender: Any) {
+        
+        if(menuShown == false)
+        {
+            leading.constant = 150
+            trailing.constant = 150
+            menuShown = true
+        }
+        else
+        {
+            leading.constant = 0
+            trailing.constant = 0
+            menuShown = false
+        }
+        
+    }
+    
 
 }
 
